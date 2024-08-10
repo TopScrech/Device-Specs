87d5acdca4db7f901813659e485621fae3e5ce24
TopScrech <sergei_saliukov@icloud.com>
2024-08-10 11:27:48 +0200

    Initial Commit

---
+++ Device Specs/Device_SpecsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Device_SpecsApp.swift
+//  Device Specs
+//
+//  Created by Sergei Saliukov on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Device_SpecsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
