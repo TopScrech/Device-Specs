0dccab2ddea192001ea3d54e9d4098561b436b4c
TopScrech <sergei_saliukov@icloud.com>
2024-09-12 09:35:14 +0200

    Display screen ratio and rounded corners

--- Device Specs/Display/Display View.swift
+++ Device Specs/Display/Display View.swift
@@ -4,18 +4,44 @@ import DeviceKit
 struct DisplaySpecs: View {
     private var display = DisplayVM()
     
+    @State private var brightness = 0.0
+    
+    init() {
+        self.brightness = Double(Device.current.screenBrightness)
+    }
+    
     var body: some View {
         List {
-#warning("fix")
+            Text(Device.current.screenRatio.height)
+            Text(Device.current.screenRatio.width)
+            
+            let isRounded = Device.current.hasRoundedDisplayCorners ? "Yes" : "No"
+            ListParameter("Rounded Corners", parameter: isRounded)
+            
             if let ppi = Device.current.ppi {
                 ListParameter("PPI", parameter: "\(ppi)")
             }
             
             ListParameter("Screen resolution", parameter: display.fetchScreenResolution())
+            ListParameter("Screen resolution", parameter: display.fetchScreenResolution())
             ListParameter("Refresh rate", parameter: display.refreshRate)
-            ListParameter("Brightness", parameter: "\(Device.current.screenBrightness)%")
+            
+            Section {
+                ListParameter("Brightness", parameter: "\(Int(brightness))%")
+                
+                Slider(value: $brightness, in: 0...100, step: 1) {
+                    Text("Brightness")
+                }
+            }
         }
         .navigationTitle("Display")
+        .onChange(of: brightness) { _, newValue in
+            setDeviceBrightness(to: CGFloat(newValue))
+        }
+    }
+    
+    private func setDeviceBrightness(to level: CGFloat) {
+        UIScreen.main.brightness = min(max(level, 0), 1)
     }
 }
 
