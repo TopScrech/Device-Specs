a4b46cce3a5ddc339d48dc42f052e2aa7656c434
TopScrech <sergei_saliukov@icloud.com>
2024-09-12 14:49:37 +0200

    Color test fix

--- Device Specs/Tests/Test List.swift
+++ Device Specs/Tests/Test List.swift
@@ -5,11 +5,11 @@ struct TestList: View {
     @State private var torch = TorchVM()
     
     private let colorTests: [String: Color] = [
-        "White": .white,
-        "Black": .black,
-        "Red": .red,
-        "Green": .green,
-        "Blue": .blue
+        "White": Color(red: 1, green: 1, blue: 1),
+        "Black": Color(red: 0, green: 0, blue: 0),
+        "Red": Color(red: 1, green: 0, blue: 0),
+        "Green": Color(red: 0, green: 1, blue: 0),
+        "Blue": Color(red: 0, green: 0, blue: 1)
     ]
     
     var body: some View {
