49b2fdc7c7c54a42982a79f5189d03990790820a
TopScrech <sergei_saliukov@icloud.com>
2024-09-13 19:12:25 +0200

    Changing the bundle of the visionOS target to finally fix Xcode Cloud

--- visionOS/App Container.swift
+++ visionOS/App Container.swift
@@ -1,15 +1,9 @@
 import SwiftUI
 
 struct AppContainer: View {
-//    @Environment(NavState.self) private var navState
-    
     var body: some View {
-//        @Bindable var binding = navState
-        
         NavigationStack {
-//        NavigationStack(path: $binding.path) {
             HomeView()
-//                .withNavDestinations()
         }
     }
 }
