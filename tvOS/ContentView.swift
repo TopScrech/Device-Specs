6955cfd5d7dc16a7cd9e3a95441f585e1730a0a6
TopScrech <sergei_saliukov@icloud.com>
2024-09-13 19:37:16 +0200

    Added a tvOS target

---
+++ tvOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tvOS
+//
+//  Created by Sergei Saliukov on 13/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
