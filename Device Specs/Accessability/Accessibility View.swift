98e0699da06a001e3e58b202430433b4c4ea94aa
TopScrech <sergei_saliukov@icloud.com>
2024-09-19 13:30:19 +0200

    Added music haptics property

--- Device Specs/Accessability/Accessibility View.swift
+++ Device Specs/Accessability/Accessibility View.swift
@@ -1,10 +1,20 @@
 import SwiftUI
+import MediaAccessibility
 
 struct AccessibilityView: View {
     @State private var vm = AccessibilityVM()
     
     var body: some View {
         List {
+            if #available(iOS 18, macCatalyst 18, visionOS 2, macOS 15, tvOS 18, *) {
+                Section {
+                    let isEnabled = MAMusicHapticsManager.shared.isActive
+                    let param = AccessibilityParam("Music Haptics", isEnabled: isEnabled)
+                    
+                    AccessibilityItem(param)
+                }
+            }
+            
             ForEach(vm.filteredParams) { param in
                 AccessibilityItem(param)
             }
