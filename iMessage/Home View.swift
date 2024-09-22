2b67466a74da00b7ce94be0a90f1696ba26e4e3f
TopScrech <sergei_saliukov@icloud.com>
2024-09-23 00:29:58 +0200

    -

--- iMessage/Home View.swift
+++ iMessage/Home View.swift
@@ -1,11 +1,95 @@
 import SwiftUI
 
 struct HomeView: View {
+//    @Environment(NavState.self) private var navState
+    
+//    @State private var battery = BatteryVM()
+//    @State private var processor = ProcessorVM()
+//    @State private var display = DisplayVM()
+//    @State private var system = SystemVM()
+//    @State private var device = DeviceVM()
+//    @State private var memory = MemoryVM()
+//    @State private var app = AppVM()
+//    @State private var connectivity = ConnectivityVM()
+    
     var body: some View {
-        Text("Hello, World!")
+        List {
+//            SpecsLink("Device", icon: "info.circle", spec: device.deviceIdentifier) {
+//                DeviceSpecs()
+//                    .environment(device)
+//            }
+//            
+//            SpecsLink("System", icon: "apple.terminal", spec: system.operatingSystem) {
+//                SystemSpecs()
+//                    .environment(system)
+//            }
+//            
+//            SpecsLink("Display", icon: "iphone", spec: display.diagonalSize) {
+//                DisplaySpecs()
+//                    .environment(display)
+//            }
+//            
+//            SpecsLink("Processor", icon: "cpu", spec: processor.cpu) {
+//                ProcessorSpecs()
+//                    .environment(processor)
+//            }
+//            
+//            SpecsLink("Memory", icon: "memorychip", spec: memory.totalRamAndDisk) {
+//                MemorySpecs()
+//                    .environment(memory)
+//            }
+//            
+//            SpecsLink("Battery", icon: "battery.100percent.bolt", spec: battery.batteryLevel) {
+//                BatterySpecs()
+//                    .environment(battery)
+//            }
+//            .symbolRenderingMode(.multicolor)
+//            
+//            SpecsLink("Network", icon: "network", spec: connectivity.type) {
+//                NetworkSpecs()
+//                    .environment(connectivity)
+//            }
+//            
+//            SpecsLink("Cameras", icon: "camera") {
+//                CameraSpecs()
+//            }
+//            
+//            SpecsLink("Sensors", icon: "barometer") {
+//                SensorsView()
+//            }
+//            
+//            SpecsLink("Accessibility", icon: "accessibility") {
+//                AccessibilityView()
+//            }
+//            
+//            Section {
+//                SpecsButton("Tests", icon: "testtube.2") {
+//                    navState.navigate(.toTests)
+//                }
+//            }
+//            
+//            Section {
+//                SpecsLink("About", icon: "questionmark.square.dashed", spec: app.versionAndBuild) {
+//                    AppSpecs()
+//                        .environment(app)
+//                }
+//            }
+        }
+        .navigationTitle("Device Specs")
     }
 }
 
 #Preview {
-    HomeView()
+    NavigationView {
+        HomeView()
+    }
+//    .environment(BatteryVM())
+//    .environment(ProcessorVM())
+//    .environment(DisplayVM())
+//    .environment(SystemVM())
+//    .environment(DeviceVM())
+//    .environment(MemoryVM())
+//    .environment(AppVM())
+//    .environment(ConnectivityVM())
+//    .environment(NavState())
 }
