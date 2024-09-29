15705f444243138a04bfa477a57154d06917a282
TopScrech <sergei_saliukov@icloud.com>
2024-09-29 16:49:30 +0200

    tvOS: Marked links to static color tests with colored circles

--- watchOS/Test List.swift
+++ watchOS/Test List.swift
@@ -1,23 +1,9 @@
 import SwiftUI
 
 struct TestList: View {
-    private let colorTests = [
-        "White": Color(red: 1, green: 1, blue: 1),
-        "Black": Color(red: 0, green: 0, blue: 0),
-        "Red": Color(red: 1, green: 0, blue: 0),
-        "Green": Color(red: 0, green: 1, blue: 0),
-        "Blue": Color(red: 0, green: 0, blue: 1)
-    ]
-    
     var body: some View {
         List {
-            Section("Static colors") {
-                ForEach(colorTests.keys.sorted(), id: \.self) { key in
-                    NavigationLink(key) {
-                        ColorView(colorTests[key]!)
-                    }
-                }
-            }
+            ColorTestList()
         }
     }
 }
