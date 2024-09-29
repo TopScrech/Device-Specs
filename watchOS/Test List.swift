3d3645966d004db78ec8acd7f8f655687ebf02c1
TopScrech <sergei_saliukov@icloud.com>
2024-09-29 17:00:05 +0200

    tvOS & watchOS: Added haptic tests

--- watchOS/Test List.swift
+++ watchOS/Test List.swift
@@ -1,9 +1,13 @@
-import SwiftUI
+import ScrechKit
 
 struct TestList: View {
     var body: some View {
         List {
             ColorTestList()
+            
+            ListLink("Haptic feedback", icon: "hand.tap") {
+                HapticTests()
+            }
         }
     }
 }
