36676a17190d2a02bc2fcf2fac1da1e6249c9a98
TopScrech <sergei_saliukov@icloud.com>
2024-10-18 12:27:41 +0200

    -

--- Device Specs/Health/Health Category Link.swift
+++ Device Specs/Health/Health Category Link.swift
@@ -1,35 +1,33 @@
-import SwiftUI
-
-struct HealthCategoryLink: View {
-    private let name: LocalizedStringKey
-    private let icon: String
-    private let records: [HealthRecord]
-    
-    init(_ name: LocalizedStringKey, icon: String, records: [HealthRecord]) {
-        self.name = name
-        self.icon = icon
-        self.records = records
-    }
-    
-    var body: some View {
-        NavigationLink {
-            RecordList(records)
-        } label: {
-            Label {
-                Text(name)
-                    .semibold()
-                    .padding(.leading, 5)
-            } icon: {
-                Image(systemName: icon)
-                    .title2()
-            }
-        }
-        .padding(.vertical, 5)
-    }
-}
-
-#Preview {
-    List {
-        HealthCategoryLink("Preview", icon: "hammer.fill", records: [])
-    }
-}
+//import SwiftUI
+//
+//struct HealthCategoryLink: View {
+//    private let name: LocalizedStringKey
+//    private let icon: String
+//    
+//    init(_ name: LocalizedStringKey, icon: String) {
+//        self.name = name
+//        self.icon = icon
+//    }
+//    
+//    var body: some View {
+//        NavigationLink {
+//            
+//        } label: {
+//            Label {
+//                Text(name)
+//                    .semibold()
+//                    .padding(.leading, 5)
+//            } icon: {
+//                Image(systemName: icon)
+//                    .title2()
+//            }
+//        }
+//        .padding(.vertical, 5)
+//    }
+//}
+//
+//#Preview {
+//    List {
+//        HealthCategoryLink("Preview", icon: "hammer.fill")
+//    }
+//}
