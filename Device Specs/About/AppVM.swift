ca52ee1e60a4af17782ccfa5a7c6cdde3b24e9da
TopScrech <sergei_saliukov@icloud.com>
2025-09-04 09:33:54 +0200

    AppVM optimized

--- Device Specs/About/AppVM.swift
+++ Device Specs/About/AppVM.swift
@@ -2,8 +2,7 @@ import Foundation
 
 @Observable
 final class AppVM {
-    private let info = ProcessInfo()
-    let bundleId = Bundle.main.bundleIdentifier ?? "-"
+    var trigger = false
     
     var version: String {
         Bundle.version ?? "-"
@@ -15,31 +14,5 @@ final class AppVM {
     
     var versionAndBuild: String {
         "v\(version) (B\(build))"
-    }
-    
-    var minimumOSVersion: String {
-        Bundle.main.object(forInfoDictionaryKey: "MinimumOSVersion") as? String ?? "Unknown"
-    }
-    
-    var trigger = false
-    
-    var isMacCatalystApp: String {
-        info.isMacCatalystApp ? "Yes" : "No"
-    }
-    
-    var isiOSAppOnMac: String {
-        info.isiOSAppOnMac ? "Yes" : "No"
-    }
-    
-    var getAppInstallationDate: String? {
-        guard
-            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first,
-            let attributes = try? FileManager.default.attributesOfItem(atPath: documentsURL.path),
-            let creationDate = attributes[.creationDate] as? Date
-        else {
-            return nil
-        }
-        
-        return creationDate.formatted()
     }
 }
