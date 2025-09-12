c40f00f270c02f56c00e42bcc0b92ff2cbfd3f8f
TopScrech <sergei_saliukov@icloud.com>
2025-09-12 11:24:32 +0200

    fixed: iphoneAir -> iPhoneAir

--- Device Specs/DeviceKit/Release Date.swift
+++ Device Specs/DeviceKit/Release Date.swift
@@ -27,7 +27,7 @@ extension Device {
         case .iPhone14, .iPhone14Plus, .iPhone14Pro, .iPhone14ProMax: "September 16, 2022"
         case .iPhone15, .iPhone15Plus, .iPhone15Pro, .iPhone15ProMax: "September 22, 2023"
         case .iPhone16, .iPhone16Plus, .iPhone16Pro, .iPhone16ProMax: "September 20, 2024"
-        case .iPhone17, .iphoneAir, .iPhone17Pro, .iPhone17ProMax: "September 9, 2025"
+        case .iPhone17, .iPhoneAir, .iPhone17Pro, .iPhone17ProMax: "September 9, 2025"
             
         case .iPad2: "March 11, 2011"
         case .iPad3: "March 16, 2012"
