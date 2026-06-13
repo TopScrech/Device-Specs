15e762fd1461a19abdcd00f12ec0c4a91e790359
TopScrech <sergei_saliukov@icloud.com>
2026-06-13 23:21:13 +0200

    Added battery tool to the assistant #45

--- iOS/Chat/ChatVM.swift
+++ iOS/Chat/ChatVM.swift
@@ -53,6 +53,7 @@ final class ChatVM {
             GetCPUInfo(),
             GetStorageInfo(),
             GetMemoryInfo(),
+            GetBatteryInfo(),
             GetCameraInfo()
         ]
 #endif
